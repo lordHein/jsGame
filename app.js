074fd6e9a1bb3a0b1ed063fa45e26cfb5f241df2
Stas <l.dior@ukr.net>
2020-04-08 20:24:52 +0300

    step 0

---
+++ app.js
@@ -0,0 +1,13 @@
+var str = '1, 2, 4, 4, 4, 9';
+
+//получить массив, 
+var array = str.split(',')
+
+console.log(array);
+
+//соединить елементы через знак
+console.log(array.join(';'));
+
+//поменять порядок
+console.log(array.reverse());
+
